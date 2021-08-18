66d966490805d7fb72a6f5c9580960490f9fe150
Sanjay Sharma <87975030+Sanjay-Sharma166@users.noreply.github.com>
2021-08-18 12:14:21 +0530

    Add files via upload

---
+++ Jscript/Bookroom.js
@@ -0,0 +1,27 @@
+function Bookroom()
+{
+    var fromdate=document.getElementById("fromdate").value;
+    var todate=document.getElementById("todate").value;
+    var purpose=document.getElementById("purpose").value;
+    var numberOfPeoples=document.getElementById("numberOfPeople").value;
+    var id=document.getElementById("employeeid").value;
+
+    if(!fromdate || !todate || !purpose)
+    {
+        alert("Booking Sucessful..");
+        return;
+    }
+
+    var show=document.getElementById("show");
+    var newRow=show.insertRow(show.length);
+    var c1=newRow.insertCell(0);
+    var c2=newRow.insertCell(1);
+    var c3=newRow.insertCell(2);
+
+    c1.innerHTML=fromdate;
+    c2.innerHTML=todate;
+    c3.innerHTML=purpose;
+    row++;
+
+
+}
---
+++ Jscript/demo.html
@@ -0,0 +1,12 @@
+<html>
+<head>
+</head>
+<body>
+<p id="demo"></p>
+
+<script>
+const cars = ["Saab", "Volvo", "BMW"];
+document.getElementById("demo").innerHTML = cars;
+</script>
+</body>
+</html>
